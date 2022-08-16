70408f8dad597182ae2701399416a5ab490d6294
AmayaClaudio84 <amayaclaudio84@gmail.com>
2022-08-16 13:31:01 -0300

    Agregados cambios para deploy

--- api/index.js
+++ api/index.js
@@ -34,8 +34,9 @@ if (process.env.PORT == 3001) {
 }
 
 // Syncing all the models at once.
+const port = process.env.PORT || 3001;
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(port, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
