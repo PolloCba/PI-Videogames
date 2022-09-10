2c88f65489300e6d9076233fbfee138399fec943
AmayaClaudio84 <amayaclaudio84@gmail.com>
2022-09-10 20:25:25 -0300

    Implementando Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,17 +4,33 @@ import stl from "./Footer.module.css";
 export default function Footer() {
   return (
     <div className={stl.containerFooter}>
-      <p className={stl.textFooter}>© Desarrollado por Claudio Amaya - 2022</p>
-      <ul>
-        <li>
-          <img src="../../../assets/linkedin.png" alt="Not found" />
-          <a href="https://www.linkedin.com/in/claudio-amaya-fullstack/">
-            LinkedIn
+      <p className={stl.textFooter}>© Developed by Claudio Amaya - 2022</p>
+      <ul className={stl.footerList}>
+        <li className={stl.footerList__item}>
+          <a
+            href="https://www.linkedin.com/in/claudio-amaya-fullstack/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className={stl.footerImg}
+              src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+              alt="Not found"
+            />
           </a>
         </li>
         <li>
-          <img src="../../../assets/github.png" alt="Not found" />
-          <a href="https://github.com/PolloCba">GitHub</a>
+          <a
+            href="https://github.com/PolloCba"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className={stl.footerImg}
+              src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+              alt="Not found"
+            />
+          </a>
         </li>
       </ul>
     </div>
